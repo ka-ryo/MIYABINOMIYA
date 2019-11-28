d2bcd88151a6820fde2bb140f360dadec34a6411
ryokato <ryo.kato0122@gmail.com>
2019-11-28 12:14:04 +0900

    no message

--- プリプロ/midi_scan.py
+++ プリプロ/midi_scan.py
@@ -23,7 +23,7 @@ def main(midi_file_name):
 
     #txt保存
     np.savetxt('{}.txt'.format(midi_file_name),one_hot_vector_midi,fmt="%2.1d")
-
+    print(one_hot_vector_midi.shape)
     return one_hot_vector_midi
 if __name__ == '__main__':
     main("Long_test.mid")
